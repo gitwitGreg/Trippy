ebd4e90c264df89a2cbdae3fc234b2fb9af18cb2
Greg <Godly@udel.edu>
2024-05-18 15:26:51 -0400

    Working on deployment

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import GoogleMaps from '../components/GoogleMaps'
 
-export default function Maps({tripId}: {tripId: string}){
+export default function Maps(){
     return(
         <GoogleMaps/>
     )
